4944aff656cb38464f863f9a12aa35a4f32493c0
Laszlo Timar <laszlo.tim0112@gmail.com>
2025-02-19 22:29:12 +0100

    implement basic CRUD operations

--- task_manager/task_app/urls.py
+++ task_manager/task_app/urls.py
@@ -1,5 +1,22 @@
+from django.urls import path
+from . import views
 
 
 urlpatterns = [
-
+    path("", views.TaskListView.as_view(), name="index"),
+    path(
+        "task/add/",
+        views.TaskCreate.as_view(),
+        name="task-add",
+    ),
+    path(
+        "task/<int:pk>/",
+        views.TaskUpdate.as_view(),
+        name="task-update",
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        views.TaskDelete.as_view(),
+        name="task-delete",
+    ),
 ]
--- task_manager/task_app/views.py
+++ task_manager/task_app/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.urls import reverse, reverse_lazy
+from .models import Task
 
-# Create your views here.
+class TaskListView(ListView):
+    model = Task
+    template_name = "task_app/index.html"
+
+    def get_queryset(self):
+        return Task.objects.filter()
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        return context
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = [
+        "title",
+        "description",
+        "due_at",
+        "status",
+    ]
+
+    def get_initial(self):
+        initial_data = super(TaskCreate, self).get_initial()
+        return initial_data
+
+    def get_context_data(self):
+        context = super(TaskCreate, self).get_context_data()
+        context["title"] = "Create a new task"
+        return context
+
+    def get_success_url(self):
+        return reverse("index")
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = [
+        "title",
+        "description",
+        "due_at",
+        "status",
+    ]
+
+    def get_context_data(self):
+        context = super(TaskUpdate, self).get_context_data()
+        context["title"] = "Edit task"
+        return context
+
+    def get_success_url(self):
+        return reverse("index")
+
+class TaskDelete(DeleteView):
+    model = Task
+
+    def get_success_url(self):
+        return reverse_lazy("index")
+
+    def get_context_data(self, **kwargs):
+        context = super(TaskDelete, self).get_context_data()
+        return context
